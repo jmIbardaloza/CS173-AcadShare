ecdc2c1aba4d3839e7af1c1cffd2582bb1e1d35e
JMIbardaloza <jcibardaloza1@up.edu.ph>
2019-11-15 10:53:47 +0800

    add database

--- AcadShare/settings.py
+++ AcadShare/settings.py
@@ -11,24 +11,26 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@$%ad0c#))746sk-n503nl3s6^n&xk(^$h6wr)%9*$g49-^1+^'
+# SECRET_KEY = '@$%ad0c#))746sk-n503nl3s6^n&xk(^$h6wr)%9*$g49-^1+^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['acadshare.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'acadshare.herokuapp.com']
 
 
 # Application definition
 
 INSTALLED_APPS = [
     'registration',
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -83,6 +85,9 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -136,3 +141,7 @@ STATICFILES_DIRS = (
 AUTH_USER_MODEL = "registration.student"
 
 LOGOUT_REDIRECT_URL = '/'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
